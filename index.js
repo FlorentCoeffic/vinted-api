920016adb46b6ac9e9f3ab6424515292659d2a83
Florent <florent@mac.lan>
2021-10-22 15:52:26 +0200

    add log

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 
 mongoose.connect(process.env.MONGODB_URI);
 
+console.log("TEST");
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
